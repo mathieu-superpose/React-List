f54396254da82f50456766a6d00f33ac59d336fd
Mathieu JOLY <m.joly@live.fr>
2021-02-07 11:14:42 +0100

    add root index

---
+++ src/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => (
+  
+    <div>
+     <h1>React-List</h1>
+    </div>
+);
+
+ReactDOM.render(<App />, document.getElementById('root'));
